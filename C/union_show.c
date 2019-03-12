d18679217e59ad3ea228854f5c0694cec6f81c10
paul.liu <paul@nichad.com>
2019-03-13 00:21:10 +0800

    新的union 类型学习

---
+++ C/union_show.c
@@ -0,0 +1,21 @@
+//
+//  union_show.c
+//  
+//
+//  Created by liufapeng on 2019/3/13.
+//
+
+#include "union_show.h"
+union Data
+{    int i ;
+    float f;
+    char str[20];
+};
+
+int main()
+{
+    union Data data;
+    printf("Memory size occupied by data : %d\n", sizeof(data));
+           return 0;
+    
+};
---
+++ C/union_show.h
@@ -0,0 +1,13 @@
+//
+//  union_show.h
+//  
+//
+//  Created by liufapeng on 2019/3/13.
+//
+
+#ifndef union_show_h
+#define union_show_h
+
+#include <stdio.h>
+
+#endif /* union_show_h */
