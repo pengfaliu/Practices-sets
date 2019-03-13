a10d574612c88353ae0122ea9c914872bfafd810
paul.liu <paul@nichad.com>
2019-03-13 14:32:53 +0800

    添加了Thread c练习目录

---
+++ C/Thread_c/Thread_c.c
@@ -0,0 +1,8 @@
+//
+//  Thread_c.c
+//  
+//
+//  Created by liufapeng on 2019/3/13.
+//
+
+#include "Thread_c.h"
---
+++ C/Thread_c/Thread_c.h
@@ -0,0 +1,13 @@
+//
+//  Thread_c.h
+//  
+//
+//  Created by liufapeng on 2019/3/13.
+//
+
+#ifndef Thread_c_h
+#define Thread_c_h
+
+#include <stdio.h>
+#include <threads.h>
+#endif /* Thread_c_h */
